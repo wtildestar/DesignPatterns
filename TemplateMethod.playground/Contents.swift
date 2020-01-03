da3ccaf25cb30166036285f010b6839c4af4e1a2
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-01-03 16:05:35 +0500

    Template Method

---
+++ TemplateMethod.playground/Contents.swift
@@ -0,0 +1,105 @@
+
+
+protocol DriveVehicle {
+    func haveASeat()
+    func useProtection()
+    func lootAtTheMirror()
+    func turnSignal()
+    func driveForward()
+    func startVehicle()
+}
+
+extension DriveVehicle {
+    func startVehicle() {
+        haveASeat()
+        useProtection()
+        lootAtTheMirror()
+        turnSignal()
+        driveForward()
+    }
+    
+    func haveASeat() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    // метод перехватчик
+    func closeTheDoor() {
+        
+    }
+    
+    func useProtection() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func lootAtTheMirror() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func turnSignal() {
+        preconditionFailure("this method should be overriden")
+    }
+    
+    func driveForward() {
+        preconditionFailure("this method should be overriden")
+    }
+}
+
+class Bicycle: DriveVehicle {
+    func haveASeat() {
+        print("sit down on a bicycle seat")
+    }
+    
+    func useProtection() {
+        print("wear a helmet")
+    }
+    
+    func lootAtTheMirror() {
+        print("look at the little mirror")
+    }
+    
+    func turnSignal() {
+        print("show left hand")
+    }
+    
+    func driveForward() {
+        print("pedal")
+    }
+}
+
+class Car: DriveVehicle {
+    func haveASeat() {
+        print("sit down on a car seat")
+        closeTheDoor()
+    }
+    
+    func closeTheDoor() {
+        print("close the door")
+    }
+    
+    func useProtection() {
+        print("fasten seat belt")
+    }
+    
+    func lootAtTheMirror() {
+        print("look at the rearview mirror")
+    }
+    
+    func turnSignal() {
+        print("turn on left turn light")
+    }
+    
+    func driveForward() {
+        print("push pedal")
+    }
+}
+
+let car = Car()
+let bicycle = Bicycle()
+
+car.startVehicle()
+print("""
+
+###########
+
+""")
+bicycle.startVehicle()
